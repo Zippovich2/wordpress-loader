3379a97375a513a82f938b474346c18183edfbbf
Roman Skoropadskyi <zipo.ckorop@gmail.com>
2020-01-22 11:27:21 +0200

    change package name in php cs fixer head

--- src/Loader.php
+++ src/Loader.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the "Wordpress Wrapper" package.
+ * This file is part of the "Wordpress Wrapper Loader" package.
  *
  * (c) Skoropadskyi Roman <zipo.ckorop@gmail.com>
  *
